f9f359c5cbd5e175d3a9175da234f36f07ad4047
tauqeerkhattak <17sw72@students.muet.edu.pk>
2023-04-13 14:58:06 +0500

    init project

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+// import * as functions from "firebase-functions";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// export const redirectUri = functions.https.onRequest(async (req, resp) => {
+//   try {
+//     const code = req.params["code"];
+//     console.log("CODE IS RECEIVED!");
+//   } catch (e) {
+//     console.log("Exception: ", e);
+//   }
+// });
